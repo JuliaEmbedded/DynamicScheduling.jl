ab60f2d3fdee560e5121aa6dba11de9222c7b820
biggsbenjamin <bb2515@ic.ac.uk>
2020-05-06 18:52:04 +0100

    vhdl test branch

---
+++ build/MANUAL_COPY_mul_graph_modelsim.tcl
@@ -0,0 +1,4 @@
+vcom -2008 /home/dynamatic/Dynamatic/etc/dynamatic/components/elastic_components.vhd
+vcom -2008 /home/dynamatic/Dynamatic/etc/dynamatic/components/arithmetic_units.vhd
+vcom -2008 /home/dynamatic/Dynamatic/etc/dynamatic/components/MemCont.vhd
+vcom -2008 hdl/MANUAL_COPY_mul_graph.vhd
