48a594372e7ccb82726b2e230d221e0c95ef0bde
biggsbenjamin <bb2515@ic.ac.uk>
2020-05-17 21:21:37 +0100

    updated tcl

--- build/mul_sim/MANUAL_COPY_mul_graph_modelsim.tcl
+++ build/mul_sim/MANUAL_COPY_mul_graph_modelsim.tcl
@@ -1,4 +1,5 @@
-vcom -2008 /home/dynamatic/Dynamatic/etc/dynamatic/components/elastic_components.vhd
-vcom -2008 /home/dynamatic/Dynamatic/etc/dynamatic/components/arithmetic_units.vhd
-vcom -2008 /home/dynamatic/Dynamatic/etc/dynamatic/components/MemCont.vhd
-vcom -2008 hdl/MANUAL_COPY_mul_graph.vhd
+vcom -2008 /home/bb2515/dynamatic/components/elastic_components.vhd
+vcom -2008 /home/bb2515/dynamatic/components/delay_buffer.vhd
+vcom -2008 /home/bb2515/dynamatic/components/arithmetic_units.vhd
+vcom -2008 /home/bb2515/dynamatic/components/MemCont.vhd
+vcom -2008 MANUAL_COPY_mul_graph.vhd
