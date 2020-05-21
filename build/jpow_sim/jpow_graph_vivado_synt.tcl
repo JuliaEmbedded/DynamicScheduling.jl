2a70890e23728681273b0b8eee77e45832478945
biggsbenjamin <bb2515@ic.ac.uk>
2020-05-21 17:55:51 +0100

    added vhds for jpow, nr testing

---
+++ build/jpow_sim/jpow_graph_vivado_synt.tcl
@@ -0,0 +1,4 @@
+read_vhdl -vhdl2008  /home/dynamatic/Dynamatic/etc/dynamatic/components/elastic_components.vhd
+read_vhdl -vhdl2008  /home/dynamatic/Dynamatic/etc/dynamatic/components/arithmetic_units.vhd
+read_vhdl -vhdl2008  /home/dynamatic/Dynamatic/etc/dynamatic/components/MemCont.vhd
+read_vhdl -vhdl2008 hdl/jpow_graph.vhd
