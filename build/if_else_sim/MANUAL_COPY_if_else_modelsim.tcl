b382c1a2242eb87ec4c229bd447d370c033de7c6
biggsbenjamin <bb2515@ic.ac.uk>
2020-05-22 14:38:19 +0100

    updated with icmp fix

--- build/if_else_sim/MANUAL_COPY_if_else_modelsim.tcl
+++
@@ -1,4 +0,0 @@
-vcom -2008 /home/dynamatic/Dynamatic/etc/dynamatic/components/elastic_components.vhd
-vcom -2008 /home/dynamatic/Dynamatic/etc/dynamatic/components/arithmetic_units.vhd
-vcom -2008 /home/dynamatic/Dynamatic/etc/dynamatic/components/MemCont.vhd
-vcom -2008 hdl/MANUAL_COPY_if_else.vhd
--- build/if_else_sim/MANUAL_COPY_if_else_vivado_synt.tcl
+++
@@ -1,4 +0,0 @@
-read_vhdl -vhdl2008  /home/dynamatic/Dynamatic/etc/dynamatic/components/elastic_components.vhd
-read_vhdl -vhdl2008  /home/dynamatic/Dynamatic/etc/dynamatic/components/arithmetic_units.vhd
-read_vhdl -vhdl2008  /home/dynamatic/Dynamatic/etc/dynamatic/components/MemCont.vhd
-read_vhdl -vhdl2008 hdl/MANUAL_COPY_if_else.vhd
